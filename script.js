e7c9699eef260fc91a277d907fb8daed2d7e8e4e
Kathleen McDaniel <kathleen_mc.daniel@yahoo.com>
2020-04-06 13:52:51 -0400

    feat: side nav menu

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuButton = document.getElementById("menu-button")
+const sideNavMenu = document.getElementById("side-nav-menu")
+const main = document.getElementById("main")
+const body = document.body
+
+function openNav() {
+  menuButton.innerHTML = "&times;";
+  menuButton.style.fontSize = "4em";
+  menuButton.style.color = "#A16C72"
+  menuButton.setAttribute("onclick", "closeNav()");
+  sideNavMenu.style.width = "300px";
+  main.style.marginRight = "300px";
+  body.style.backgroundColor = "rgba(20, 120, 90, 0.6)";
+}
+
+function closeNav() {
+  menuButton.innerHTML = "&#9776";
+  menuButton.style.fontSize = "3em";
+  menuButton.style.color = "rgb(101, 197, 176)"
+  menuButton.setAttribute("onclick", "openNav()");
+  sideNavMenu.style.width = "0";
+  main.style.marginRight= "0";
+  body.style.backgroundColor = "#BFFFF1";
+}
